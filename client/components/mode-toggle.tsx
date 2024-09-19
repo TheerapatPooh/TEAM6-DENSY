e7735e4c12b9b12809d70e71fbb1a923e511b705
TheerapatPooh <65160224@go.buu.ac.th>
2024-09-19 23:48:55 +0700

    <👷 build>[client:components]:{add component}

--- client/components/mode-toggle.tsx
+++ client/components/mode-toggle.tsx
@@ -15,12 +15,12 @@ export default function ModeToggle() {
     return (
         <DropdownMenu>
             <DropdownMenuTrigger>
-                <div className="w-[45px] h-[45px] outline-none rounded-md flex justify-center items-center">
-                    <span className="material-symbols-outlined rotate-0  scale-100 dark:scale-0 dark:-rotate-90 bg-transparent">light_mode</span>
-                    <span className="material-symbols-outlined absolute rotate-90 scale-0 dark:scale-100 dark:rotate-0 bg-transparent">dark_mode</span>
+                <div className="w-[45px] h-[45px] outline-none rounded-md flex justify-center items-center bg-slate-50  hover:bg-slate-200">
+                    <span className="material-symbols-outlined rotate-0  scale-100 dark:scale-0 dark:-rotate-90">light_mode</span>
+                    <span className="material-symbols-outlined absolute rotate-90 scale-0 dark:scale-100 dark:rotate-0">dark_mode</span>
                 </div>
             </DropdownMenuTrigger>
-            <DropdownMenuContent align="end">
+            <DropdownMenuContent align="end" className='p-0'>
                 <DropdownMenuItem onClick={() => setTheme("light")}>
                     Light
                 </DropdownMenuItem>
