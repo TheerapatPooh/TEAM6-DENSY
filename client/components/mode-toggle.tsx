1de9cebc8473ff8293290b7e052a7094c87e6a56
TheerapatPooh <65160224@go.buu.ac.th>
2024-09-20 23:02:24 +0700

    <💄 style>[client:component]:{update color mode-toggle}

--- client/components/mode-toggle.tsx
+++ client/components/mode-toggle.tsx
@@ -1,24 +1,34 @@
 'use client'
 import { useTheme } from 'next-themes'
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import {
     DropdownMenu,
     DropdownMenuContent,
     DropdownMenuItem,
     DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu"
+import { Button } from '@/components/ui/button'
 
 
 export default function ModeToggle() {
-    const { setTheme } = useTheme()
+    const { setTheme, theme, resolvedTheme } = useTheme()
+    const [mounted, setMounted] = useState(false)
+    
+    useEffect(() => {
+        setMounted(true)
+    }, [])
+
+    if (!mounted) {
+        return null
+    }
 
     return (
         <DropdownMenu>
             <DropdownMenuTrigger>
-                <div className="w-[45px] h-[45px] outline-none rounded-md flex justify-center items-center bg-slate-50  hover:bg-slate-200">
+                <Button variant='ghost' className="w-[45px] h-[45px]">
                     <span className="material-symbols-outlined rotate-0  scale-100 dark:scale-0 dark:-rotate-90">light_mode</span>
                     <span className="material-symbols-outlined absolute rotate-90 scale-0 dark:scale-100 dark:rotate-0">dark_mode</span>
-                </div>
+                </Button>
             </DropdownMenuTrigger>
             <DropdownMenuContent align="end" className='p-0'>
                 <DropdownMenuItem onClick={() => setTheme("light")}>
