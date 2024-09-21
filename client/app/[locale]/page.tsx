bf3e3c58b5afc7faf2e4060b6e9fdec7a633e69e
65160319KitikawinHompan <65160319@go.buu.ac.th>
2024-09-22 01:08:06 +0700

    <lipstick style>[client:component]:{Make patrol-card component}

--- client/app/[locale]/page.tsx
+++ client/app/[locale]/page.tsx
@@ -1,6 +1,13 @@
 import LanguageSelect from '@/components/language-select';
 import ModeToggle from '@/components/mode-toggle';
+import { CreatePatrolCard, PatrolCard } from '@/components/patrol-card';
 import {useTranslations} from 'next-intl';
+
+enum patrolStatus {
+  scheduled = "Scheduled",
+  onGoing = "On Going",
+  completed = "Completed",
+}
  
 export default function HomePage() {
   const t = useTranslations('PatrolPage');
@@ -9,6 +16,39 @@ export default function HomePage() {
       <ModeToggle />
       <LanguageSelect />
       <h1>{t('greeting')}</h1>
+      <div className="flex p-2 gap-4">
+      <CreatePatrolCard />
+      <PatrolCard 
+        patrolStatus= { patrolStatus.scheduled }
+        patrolDate={ new Date('2024-06-21') } 
+        patrolTitle="General Inspection" 
+        patrolPreset="P08001"  
+        patrolorName="John Doe" 
+        patrolAllItems={ 0 }
+        patrolAllComments={ 0 }
+        patrolAllDefects={ 0 }
+      />
+       <PatrolCard 
+        patrolStatus= { patrolStatus.onGoing }
+        patrolDate={ new Date('2024-05-20') } 
+        patrolTitle="General Inspection" 
+        patrolPreset="P08001"  
+        patrolorName="John Doe" 
+        patrolAllItems={ 3 }
+        patrolAllComments={ 1 }
+        patrolAllDefects={ 0 }
+      />
+       <PatrolCard 
+        patrolStatus= { patrolStatus.completed } 
+        patrolDate={ new Date('2024-04-21') } 
+        patrolTitle="General Inspection" 
+        patrolPreset="P08001"  
+        patrolorName="John Doe" 
+        patrolAllItems={ 15 }
+        patrolAllComments={ 2 }
+        patrolAllDefects={ 3 }
+      />
+      </div>
     </div>
   );
 }
