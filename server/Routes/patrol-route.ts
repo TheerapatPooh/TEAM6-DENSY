110c895fac891a2ad20c21fd1f22d8d3a17752a6
ApinopApinyachon <152172069+ApinopApinyachon@users.noreply.github.com>
2024-09-28 23:21:13 +0700

    <♻️ refactor>[server: Controllers, server: Routes]:{change name variable and function}

--- server/Routes/patrol-route.ts
+++ server/Routes/patrol-route.ts
@@ -1,9 +1,9 @@
-import { createPatrols, getAllPatrols, getPatrol } from "../Controllers/patrol-controller";
+import { createPatrol, getAllPatrols, getPatrol } from "../Controllers/patrol-controller";
 import { Router } from 'express'
 const router = Router()
 
 router.get('/patrols', getAllPatrols)
 router.get('/patrol/:id', getPatrol)
-router.post('/patrols', createPatrols)
+router.post('/patrols', createPatrol)
 
 module.exports = router
--- server/Routes/user-route.ts
+++ server/Routes/user-route.ts
@@ -1,9 +1,9 @@
-import { getUser, getAllUsers, createUsers } from "../Controllers/user-controller";
+import { getUser, getAllUsers, createUser } from "../Controllers/user-controller";
 import { Router } from 'express'
 const router = Router()
 
 router.get('/users/:id', getUser)
 router.get('/users', getAllUsers)
-router.post('/users', createUsers)
+router.post('/users', createUser)
 
 module.exports = router
