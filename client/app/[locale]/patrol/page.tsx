c1193a2ce4635b68cab8df543b84a100fc6ca64b
65160319KitikawinHompan <65160319@go.buu.ac.th>
2024-09-29 00:06:22 +0700

    <🐛 fix>[client,component]:{change pending status color and make it can click and hover to show inspector names in page made div be grid}

--- client/app/[locale]/patrol/page.tsx
+++ client/app/[locale]/patrol/page.tsx
@@ -17,48 +17,48 @@ export default function HomePage() {
     <div className='flex flex-col p-5 gap-y-5'>
       <h1>{t('greeting')}</h1>
       <Textfield iconName='search' showIcon={true} placeholder='Search...'/>
-      <div className="flex gap-4">
-      <CreatePatrolCard />
-      <PatrolCard 
-        patrolSheetStatus= { patrolSheetStatus.pending }
-        patrolSheetDate={ new Date('2024-06-19') } 
-        patrolSheetTitle="General Inspection" 
-        presetNumber="P08001"  
-        inspectorNames={["John Doe", "Jane Smith"]}
-        detectedItems={ 0 }
-        detectedComments={ 0 }
-        detectedDefects={ 0 }
-      />
-      <PatrolCard 
-        patrolSheetStatus= { patrolSheetStatus.scheduled }
-        patrolSheetDate={ new Date('2024-06-21') } 
-        patrolSheetTitle="General Inspection" 
-        presetNumber="P08001"  
-        inspectorNames={["John Doe"]}  
-        detectedItems={ 0 }
-        detectedComments={ 0 }
-        detectedDefects={ 0 }
-      />
-       <PatrolCard 
-        patrolSheetStatus= { patrolSheetStatus.onGoing }
-        patrolSheetDate={ new Date('2024-06-21') } 
-        patrolSheetTitle="General Inspection" 
-        presetNumber="P08001"  
-        inspectorNames={["Ethan Blake", "Mia Johnson", "Lucas Harper", "Ava Mitchell", "Noah Carter"]}
-        detectedItems={ 3 }
-        detectedComments={ 1 }
-        detectedDefects={ 0 }
-      />
-       <PatrolCard 
-        patrolSheetStatus= { patrolSheetStatus.completed } 
-        patrolSheetDate={ new Date('2024-5-21') } 
-        patrolSheetTitle="General Inspection" 
-        presetNumber="P08001"  
-        inspectorNames={["Ethan Blake", "Mia Johnson", "Lucas Harper", "Ava Mitchell", "Noah Carter", "Sophia Davis", "Liam Thompson"]}
-        detectedItems={ 15 }
-        detectedComments={ 2 }
-        detectedDefects={ 3 }
-      />
+      <div className="grid sm:grid-cols-2 lg:grid-cols-4 gap-4">
+        <CreatePatrolCard />
+        <PatrolCard 
+          patrolSheetStatus= { patrolSheetStatus.pending }
+          patrolSheetDate={ new Date('2024-06-19') } 
+          patrolSheetTitle="General Inspection" 
+          presetNumber="P08001"  
+          inspectorNames={["John Doe", "Jane Smith"]}
+          detectedItems={ 0 }
+          detectedComments={ 0 }
+          detectedDefects={ 0 }
+        />
+        <PatrolCard 
+          patrolSheetStatus= { patrolSheetStatus.scheduled }
+          patrolSheetDate={ new Date('2024-06-21') } 
+          patrolSheetTitle="General Inspection" 
+          presetNumber="P08001"  
+          inspectorNames={["John Doe"]}  
+          detectedItems={ 0 }
+          detectedComments={ 0 }
+          detectedDefects={ 0 }
+        />
+        <PatrolCard 
+          patrolSheetStatus= { patrolSheetStatus.onGoing }
+          patrolSheetDate={ new Date('2024-06-21') } 
+          patrolSheetTitle="General Inspection" 
+          presetNumber="P08001"  
+          inspectorNames={["Ethan Blake", "Mia Johnson", "Lucas Harper", "Ava Mitchell", "Noah Carter"]}
+          detectedItems={ 3 }
+          detectedComments={ 1 }
+          detectedDefects={ 0 }
+        />
+        <PatrolCard 
+          patrolSheetStatus= { patrolSheetStatus.completed } 
+          patrolSheetDate={ new Date('2024-5-21') } 
+          patrolSheetTitle="General Inspection" 
+          presetNumber="P08001"  
+          inspectorNames={["Ethan Blake", "Mia Johnson", "Lucas Harper", "Ava Mitchell", "Noah Carter", "Sophia Davis", "Liam Thompson"]}
+          detectedItems={ 15 }
+          detectedComments={ 2 }
+          detectedDefects={ 3 }
+        />
       </div>
       <BadgeCustom 
         iconName="check_circle" 
