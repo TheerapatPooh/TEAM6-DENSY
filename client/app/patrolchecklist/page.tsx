ef31f70a730833bf4e77a6b0db3fe420b7e3123d
inthegarden <65160357@go.buu.ac.th>
2024-10-04 21:50:38 +0700

    <✅ test>[client:app]:{Delete Patrol Checklist Page}

--- client/app/patrolchecklist/page.tsx
+++
@@ -1,9 +0,0 @@
-import PatrolChecklist from "@/components/patrol-checklist"
-
-export default function Page() {
-  return(
-    <div className="">
-        <PatrolChecklist id={0} title={""} description={""} version={0} latest={false} updated_at={""} updated_by={0} checklists={[]}/>
-    </div>
-  );
-}
