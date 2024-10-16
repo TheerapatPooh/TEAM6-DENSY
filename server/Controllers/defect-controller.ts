3c9bccf0c63560d926fca5729c63861c3576d3de
PacharaNokroy <65160342@go.buu.ac.th>
2024-10-16 22:35:24 +0700

    <✨add>[server:controller & route]{updateDefect, getDefect, getAllDefect, deleteDefect}

--- server/Controllers/defect-controller.ts
+++ server/Controllers/defect-controller.ts
@@ -1,3 +1,4 @@
+import { de } from '@faker-js/faker/.';
 import  { prisma } from '../Utils/database'
 import  { Request, Response } from 'express'
 
@@ -20,3 +21,62 @@ export async function createDefect(req: Request, res: Response) {
         res.status(500).send(err)
     }
 }
+export async function getDefect(req: Request, res: Response) {
+    try {
+        const { id } = req.params; 
+
+        const defect = await prisma.defect.findUnique({
+            where: {
+                df_id: Number(id), 
+            },
+        });
+        res.status(200).json(defect);
+    } catch (err) {
+        res.status(500).send(err);
+    }
+}
+
+export async function getAllDefect(req: Request, res: Response) {
+    try {
+        const defects = await prisma.defect.findMany();
+        res.status(200).json(defects);
+    } catch (err) {
+        res.status(500).send(err);
+    }
+}
+
+export async function updateDefect(req: Request, res: Response) {
+    try {
+        const { id } = req.params;
+        const { name, description, type, status, userId } = req.body;
+        const defect = await prisma.defect.update({
+            where: {
+                df_id: Number(id),
+            },
+            data: {
+                df_name: name,
+                df_description: description,
+                df_type: type,
+                df_status: status,
+                df_us_id: userId
+            },
+        });
+        res.status(200).json(defect);
+    } catch (err) {
+        res.status(500).send(err);
+    }
+}   
+
+export async function deleteDefect(req: Request, res: Response) {
+    try {
+        const { id } = req.params;
+        const defect = await prisma.defect.delete({
+            where: {
+                df_id: Number(id),
+            },
+        });
+        res.status(200).json(defect);
+    } catch (err) {
+        res.status(500).send(err);
+    }
+}       
--- server/Routes/defect-route.ts
+++ server/Routes/defect-route.ts
@@ -1,7 +1,11 @@
-import { createDefect } from "../Controllers/defect-controller";
+import { createDefect,getDefect, getAllDefect, deleteDefect, updateDefect,} from "../Controllers/defect-controller";
 import { Router } from 'express'
 const router = Router()
 
 router.post('/defect', createDefect)
+router.get('/defect/:id', getDefect)
+router.get('/defect',  getAllDefect)
+router.put('/defect/:id', updateDefect)
+router.delete('/defect/:id', deleteDefect)
 
 module.exports = router
