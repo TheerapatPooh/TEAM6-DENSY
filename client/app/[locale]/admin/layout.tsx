71ea9cd6c6232e51b87e2560a33da4108210e263
Kitikawin Hompan <65160319@go.buu.ac.th>
2024-12-16 22:26:41 +0700

    <🐛fix>[client:component:]:{make it can expand} && <🐛fix>[client:app:[local]:admin:layout]:{fix it to make div header and children move when expand sidebar}

--- client/app/[locale]/admin/layout.tsx
+++ client/app/[locale]/admin/layout.tsx
@@ -8,8 +8,13 @@ export default function AdminLayout({
   }>) {
     return (
     <>
-      <Header variant="admin"/>
-      {children}
+      <div className="flex flex-row h-screen">
+        <AdminSidebar />
+        <div className="flex-grow overflow-x-hidden overflow-y-auto">
+          <Header variant="admin"/>
+          {children}
+        </div>
+      </div>
     </>
     ); 
   }
--- client/components/admin-sidebar.tsx
+++ client/components/admin-sidebar.tsx
@@ -17,55 +17,65 @@ import darkLogo from "@/public/assets/img/system_logo_dark.png"
 import Image from 'next/image'
 import { useTheme } from 'next-themes'
 import { Button } from "@/components/ui/button";
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import { useTranslations } from "next-intl";
 
 export function AdminSidebar() {
-    const sidebarItems = [
+    const locale = useLocale()
+    const router = useRouter()
+    const { resolvedTheme } = useTheme()
+    const [isExpanded, setIsExpanded] = useState(false);
+    const [mounted, setMounted] = useState(false);
+    const t = useTranslations("Sidebar");
+
+    const sidebarItems = [ // ชุดข้อมูลของปุ่มตัวเลือกใน Sidebar
         {
-            title: "Dashboard",
+            title: t("Dashboard"),
             url: "#",
             icon: "browse",
         },
         {
-            title: "Employees",
+            title: t("Employees"),
             url: "#",
             icon: "manage_accounts",
         },
         {
-            title: "Settings",
+            title: t("Settings"),
             url: "#",
             icon: "manufacturing",
         },
     ]
 
-    const locale = useLocale()
-    const router = useRouter()
-    const { resolvedTheme } = useTheme()
-    const [isExpanded, setIsExpanded] = useState(true);
+    useEffect(() => {
+        setMounted(true);
+      }, []);
+      if (!mounted) {
+        return null;
+      }
 
-    const toggleSidebar = () => {
+    const toggleSidebar = () => { 
         setIsExpanded((prev) => !prev);
-      };
+    };
 
     return (
-        <SidebarProvider className={`custom-shadow sticky transition-all duration-300 ${isExpanded ? "w-[240px]" : "w-[70px]"}`}>
+        <SidebarProvider className={`sticky transition-all duration-300 ${isExpanded ? "w-[240px]" : "w-[70px]"}`}>
              <Sidebar className={`w-[240px] border-none ${isExpanded ? "w-[240px]" : "w-[70px]"}`}>
-                <SidebarContent className="p-[8px] pt-[8px] bg-card">
+                <SidebarContent className="p-[8px] pt-[8px] bg-card overflow-x-hidden">
                 <SidebarGroup className="p-0">
                     <SidebarGroupLabel className="mb-[16px]">
                         <div className="flex items-center space-x-[24px] mt-[16px] transition-all duration-300">
                             { isExpanded && (
-                            <Image
-                                className="flex items-center cursor-pointer"
-                                src={resolvedTheme === 'dark' ? darkLogo : lightLogo}
-                                alt="Logo"
-                                width={140}
-                                height={35}
-                                priority
-                                onClick={() => router.push(`/${locale}`)}
-                            />
+                                <Image
+                                    className="flex items-center cursor-pointer"
+                                    src={resolvedTheme === 'dark' ? darkLogo : lightLogo}
+                                    alt="Logo"
+                                    width={140}
+                                    height={35}
+                                    priority
+                                    onClick={() => router.push(`/${locale}`)}
+                                />
                             )}
-                            <Button variant="ghost" className="size-[54px] text-muted-foreground transition-all duration-300 ms-[-8px]" onClick={toggleSidebar}>
+                            <Button variant="ghost" className="size-[45px] text-muted-foreground transition-all duration-300 ms-[-4px]" onClick={toggleSidebar}>
                                 <span className="material-symbols-outlined text-10">menu</span>
                             </Button>
                         </div>
@@ -75,10 +85,10 @@ export function AdminSidebar() {
                         {sidebarItems.map((item) => (
                         <SidebarMenuItem key={item.title}>
                             <SidebarMenuButton asChild>
-                                <div onClick={() => router.push(`/${locale}/admin/${item.url}`)} className={`w-[224px] h-[50px] transition-all duration-300 text-muted-foreground hover:bg-accent-gradient hover:text-background cursor-pointer ${isExpanded ? "justify-start" : "justify-center"}`}>
+                                <div onClick={() => router.push(`/${locale}/admin/${item.url}`)} className={`w-[224px] h-[50px] transition-all duration-300 text-muted-foreground hover:bg-accent-gradient hover:text-secondary cursor-pointer ${isExpanded ? "justify-start" : "justify-center"}`}>
                                     <span className="material-symbols-outlined">{item.icon}</span>
                                     {isExpanded && (
-                                        <span className="text-[18px] w-[95px] h-[22px]">{item.title}</span>
+                                        <span className="text-lg w-auto">{item.title}</span>
                                     )}
                                 </div>
                             </SidebarMenuButton>
