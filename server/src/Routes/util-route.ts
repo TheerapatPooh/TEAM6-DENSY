fc27ade3ee397be0d04280c31fcedeaab482d8b8
TheerapatPooh <65160224@go.buu.ac.th>
2025-01-03 02:54:17 +0700

    <✨add>[client: components]:{add remove notification}

--- server/src/Routes/util-route.ts
+++ server/src/Routes/util-route.ts
@@ -1,4 +1,4 @@
-import { authenticateUser,removeOldNotifications, getNotifications, login, logout, markAllAsRead, updateNotification } from "@Controllers/util-controller.js";
+import { authenticateUser,removeOldNotifications, getNotifications, login, logout, markAllAsRead, updateNotification, removeNotification, removeAllNotifications } from "@Controllers/util-controller.js";
 import { Router } from "express";
 
 const router = Router();
@@ -7,6 +7,8 @@ router.post("/login", login);
 router.post("/logout", logout);
 router.get("/notifications", authenticateUser, getNotifications);
 router.put("/notification/:id", authenticateUser, updateNotification);
+router.delete("/notification/:id", authenticateUser, removeNotification);
+router.delete("notifications", authenticateUser, removeAllNotifications);
 router.put("/notifications/mark-all-read", authenticateUser, markAllAsRead);
 removeOldNotifications()
 
