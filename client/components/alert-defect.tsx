467943f268d03c5e44b9673e29f0cd9ebb1aed31
TheerapatPooh <65160224@go.buu.ac.th>
2025-02-09 02:43:18 +0700

    <💄 style>[client: components]:{fix scroll for image}

--- client/components/alert-defect.tsx
+++ client/components/alert-defect.tsx
@@ -47,7 +47,7 @@ import {
 import { Skeleton } from './ui/skeleton';
 import { toast } from '@/hooks/use-toast';
 import { AlertCustom } from "@/components/alert-custom";
-import { tree } from 'next/dist/build/templates/app-page';
+import { AspectRatio } from "@/components/ui/aspect-ratio";
 
 interface IAlertDefect {
     defect?: IDefect,
@@ -439,12 +439,12 @@ export default function AlertDefect({ defect, item, type, patrolResults, result,
                             </div>
                         }
 
-                        <div className="flex flex-col justify-between w-full">
+                        <div className="flex flex-col gap-1 justify-between w-full">
                             <div className='text-sm font-semibold'>
                                 {t("Image")}
                             </div>
-                            <div className='flex flex-row py-1 pr-2 gap-4'>
-                                <div className='flex flex-col gap-1 flex-1 rounded-md custom-shadow'>
+                            <div className='grid grid-cols-2 pr-2 gap-4'>
+                                <AspectRatio ratio={4 / 3} className="flex flex-col gap-1 flex-1 rounded-md custom-shadow">
                                     <div
                                         className="flex h-full w-full rounded-[10px] bg-secondary justify-center items-center"
                                         onDragOver={handleDragOver}
@@ -481,14 +481,14 @@ export default function AlertDefect({ defect, item, type, patrolResults, result,
                                             </div>
                                         </div>
                                     </div>
-                                </div>
-                                <ScrollArea className="h-72 overflow-y-auto gap-5 rounded-md flex-1">
+                                </AspectRatio>
+                                <ScrollArea className="sm:h-64 md:h-72 lg:h-96 overflow-y-auto gap-5 rounded-md flex-1">
                                     <div className="flex flex-col gap-2 w-full">
                                         {selectedFiles?.map((file, index) => (
                                             <div key={index} className=" flex flex-row justify-between px-4 py-2 w-full bg-secondary rounded-md">
                                                 <div className='flex flex-row gap-3'>
                                                     <div className='flex flex-col justify-center items-center'>
-                                                        <span className="material-symbols-outlined">
+                                                        <span className="material-symbols-outlined text-card-foreground">
                                                             image
                                                         </span>
                                                     </div>
@@ -497,7 +497,7 @@ export default function AlertDefect({ defect, item, type, patrolResults, result,
                                                             <TooltipProvider>
                                                                 <Tooltip>
                                                                     <TooltipTrigger asChild>
-                                                                        <div className=" truncate w-[145px]">
+                                                                        <div className=" truncate sm:w-56 lg:w-72">
                                                                             {file.name}
                                                                         </div>
                                                                     </TooltipTrigger>
@@ -622,7 +622,6 @@ export default function AlertDefect({ defect, item, type, patrolResults, result,
                     </AlertDialogContent>
                 }
             </AlertDialog>
-
         </div >
     )
 }
